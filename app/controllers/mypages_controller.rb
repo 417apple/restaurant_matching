13ac7bb50ab76f583959fa8ec1f28d79cd25e836
鈴木優里 <suzukiyuri@suzukiyurinoMacBook-Air.local>
2019-10-18 21:15:44 +0900

    fourth commit

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -2,8 +2,12 @@ class MypagesController < ApplicationController
   def show
     @restaurants = current_user.restaurants.order(id: "DESC")
     @like = current_user.liked_restaurants
-  
-
+    if current_user.followings.exists?
+      @follows = current_user.followings
+    end
+    if current_user.followers.exists?
+      @followers = current_user.followers
+    end 
   end
 
 
