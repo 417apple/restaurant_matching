bb7a57d52464d65f3464e7956e9959eea8c8a088
鈴木優里 <suzukiyuri@suzukiyurinoMacBook-Air.local>
2019-10-19 17:37:06 +0900

    mount ImageUploaderを変えた

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,7 +5,6 @@ class Restaurant < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user, dependent: :destroy
-  mount_uploader :image, ImageUploader
   geocoded_by :address
   after_validation :geocode
 
