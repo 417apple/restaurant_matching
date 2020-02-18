a92737553eab1cc67fa64359d95d948602629625
鈴木優里 <lil.yf.bb7v@gmail.com>
2020-02-18 17:22:19 +0900

    followボタンを非同期通信に変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,10 +7,10 @@ class RelationshipsController < ApplicationController
     @following = current_user.follow(@user)
     if @following.save
       flash[:success] = 'ユーザーをフォローしました'
-      redirect_to controller: 'users', action: 'show', id: @user.id
+
     else
       flash.now[:alert] = 'ユーザーのフォローに失敗しました'
-      redirect_to controller: 'users', action: 'show', id: @user.id
+
     end
   end
 
@@ -20,10 +20,10 @@ class RelationshipsController < ApplicationController
     @following = current_user.unfollow(@user)
     if @following.destroy
       flash[:success] = 'ユーザーのフォローを解除しました'
-      redirect_to controller: 'users', action: 'show', id: @user.id,:notice => "成功"
+
     else
       flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
-      redirect_to controller: 'users', action: 'show', id: @user.id ,:notice => "失敗"
+
     end
   end
 
