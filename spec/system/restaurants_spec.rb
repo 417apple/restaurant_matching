961bfd8b9b65f0fa3eaa31df466d149d3a05eb0c
鈴木優里 <lil.yf.bb7v@gmail.com>
2020-02-23 12:03:14 +0900

    systemspec編集

--- spec/system/restaurants_spec.rb
+++ spec/system/restaurants_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 describe 'レストラン投稿機能', type: :system do
   describe '一覧表示機能' do
     before do
-      user_a = create(:user)
-      create(:restaurant)
+      user_a = create(:user, name: 'ユーザーA', email: 'a@example.com',password:'aaaaaa',  password_confirmation:'aaaaaa')
+      create(:restaurant, name: '最初のテスト店',user: user_a)
     end
 
     context 'ユーザーAがログインしているとき' do
       before do
         visit new_user_session_path
-        fill_in 'email', with: 'a@example.com'
-        fill_in 'password', with: 'aaaaaa'
+        fill_in 'Email', with: 'a@example.com'
+        fill_in 'Password', with: 'aaaaaa'
         click_button 'Log in'
       end
 
       it 'ユーザーAが作成したレストランが表示される' do
-        expect(page).to have_content '最初の店名'
+        expect(page).to have_content '最初のテスト店'
       end
     end
   end
